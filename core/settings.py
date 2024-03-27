8233ec5dc923d1065f9919992285af5fb296f25a
Betzyc <43485138+Betzyc@users.noreply.github.com>
2024-03-27 13:09:09 -0600

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -112,12 +112,12 @@ DATABASES = {
 
 # Se configuran las opciones de CORS
     
-    CORS_ALLOWED_ORIGINS = [
-        'http://localhost:5173',
-        'http://127.0.0.1:5173',
-        'http://localhost:3000',
-        'http://127.0.0.1:3000',
-    ]
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:5173',
+    'http://127.0.0.1:5173',
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
+]
 
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:3000',
